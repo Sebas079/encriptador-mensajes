b300c55ba9d5878122f8d28c225c2f7759bb87c4
Sebastian <77014709+Sebas079@users.noreply.github.com>
2022-01-26 19:03:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,68 @@
-// obtener mensaje
-/*const mensajeUsuario = document.getElementsByClassName(".mensajeUsuario").value;
-const btnEncriptar = document.querySelectorAll("#btn-encriptar");
-const btnDesencriptar = document.querySelectorAll("#btn-Desencriptar");
-const btncopiar = document.querySelectorAll("#btn-copiar");
-const salidaMensaje = document.getElementsByClassName(".salidaMensaje");
-console.log(mensajeUsuario);*/
-//validar mensaje
-
-
-
-/*function validarMensaje() {
-    if (mensajeUsuario.value == /(A-Z 0-9 ´)/gi) {
-        return alert("Mensaje invalido solo minusculas sin acento!!");
-    }
-}*/
-
 // encriptar mensaje
-/*function encriptarMensaje() {
-    // validarMensaje();
-    const msj = mensajeUsuario.value;
-    const msjEncrip = msj.replace(/a/gi, "ei").replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+function encriptarMensaje() {
 
-    salidaMensaje.value = msjEncrip;
-    mensajeUsuario.value =" ";
+    var encriptar = document.getElementById('mensajeUsuario').value;
+    
+    var encriptado = encriptar.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+    document.getElementById("salidaMensaje").value = encriptado;
+    // document.getElementById("mensajeUsuario").value = " ";
+    console.log(encriptado);
+
+
 
 };
-function encriptar(){
-    const encriptar = document.getElementById('entrada_texto').value;
-    const encriptado = encriptar.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
-    document.getElementById("salidaMensaje").value = encriptado
-    document.getElementById("mensajeUsuario").value=" ";
+// desencriptar mensaje
+function desencriptarMensaje() {
+
+    var desencriptar = document.getElementById('mensajeUsuario').value;
+    var desencriptado = desencriptar.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+    document.getElementById("salidaMensaje").value = desencriptado;
+    console.log(desencriptado);
+    // document.getElementById("mensajeUsuario").value = " ";
+};
+//validar texto
+function validarTexto() {
+     
+    let validar =document.getElementById('mensajeUsuario').value ;
+    if (validar.toUpperCase()) {
+         validar.value ="Solo letras minusculas sin acento";
+         return document.getElementById("mensajeUsuario").value = validar;
+
+    }
+
+
+
 }
 
-//Desencriptar mensaje
-/*function desencriptarMensaje() {
-    let msjDesenc = mensajeUsuario.value;
-    let resultado = msjDesenc.replace(/ei/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
-    salidaMensaje.value = r;
-
-}*/
 
 
-function encriptarMensaje(){
-    const encriptar = document.getElementById('entrada_texto').value;
-    const encriptado = encriptar.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
-    document.getElementById("salida_texto").value = encriptado
-    document.getElementById("entrada_texto").value=" ";
-}
 
 
-function desencriptarMansaje(){
-    const desencriptar= document.getElementById ("entrada_texto").value;
-    const desencriptado= desencriptar.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
-    document.getElementById("salida_texto").value = desencriptado
-    document.getElementById("entrada_texto").value=" ";
-}
+//boton encriptar
+
+var btnEncriptar = document.querySelector("#btn-encriptar");
+btnEncriptar.addEventListener("click", function (event) {
+    event.preventDefault();
+    validarTexto();
+    encriptarMensaje();
+
+});
+// boton desencriptar
+var btnDesencriptar = document.querySelector("#btn-desencriptar");
+btnDesencriptar.addEventListener("click", function (event) {
+    event.preventDefault();
+    desencriptarMensaje();
+
+
+});
+// boton copiar
+var btnCopiar = document.querySelector("#btn-copiar");
+btnCopiar.addEventListener("click", function (event) {
+    event.preventDefault();
+    let copiar = document.getElementById("salidaMensaje");
+    copiar.focus();
+    document.execCommand("selectAll");
+    document.execCommand("copy")
+
+});
+
